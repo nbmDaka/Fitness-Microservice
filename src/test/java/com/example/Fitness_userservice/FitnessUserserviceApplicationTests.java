adeb974599857d3eaf5224ee8c27ba4f53d9b733
nbmdaka <nurdaulet.beketov.2005@gmail.com>
2025-08-05 16:50:12 +0500

    Again first commit

---
+++ src/test/java/com/example/Fitness_userservice/FitnessUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fitness_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
