d67bfab2307d97152106372ef2813c53ea6a1050
Nurdaulet <nurdaulet.beketov.2005@gmail.com>
2025-08-10 11:50:14 +0500

    Finishing creating user endpoints

--- src/main/java/com/example/Fitness_userservice/dto/UserResponse.java
+++ src/main/java/com/example/Fitness_userservice/dto/UserResponse.java
@@ -14,7 +14,6 @@ import java.time.LocalDateTime;
 public class UserResponse {
     private String id;
     private String email;
-    private String password;
     private String firstName;
     private String lastName;
     private LocalDateTime createdAt;
