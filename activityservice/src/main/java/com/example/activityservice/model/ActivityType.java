de63f8e1f7ffa6974a292a5daf345b7f89483171
Nurdaulet <nurdaulet.beketov.2005@gmail.com>
2025-08-11 23:43:09 +0500

    Starting modeling activity service

---
+++ activityservice/src/main/java/com/example/activityservice/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.example.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT, // High-Intensity Interval Training
+    CARDIO,
+    STRETCHING,
+    OTHERS // For any other activity not listed
+}
